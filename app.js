eb8d77d91e5e1b349ccbb44b0183959d6559fbe5
duhanbayrak <duhanbayrak@Duhan-MacBook-Air.local>
2021-12-22 15:45:13 +0300

    first commit

--- app.js
+++ app.js
@@ -199,4 +199,4 @@ app.get('/usa', function (req, res) {
 
 })
 
-app.listen(PORT, () => console.log("Example app listening on port port!"));
+//app.listen(PORT, () => console.log("Example app listening on port port!"));
